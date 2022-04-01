3c3800f3166a92329dec696fed81818e3ed441dc
1nf0rmatix <102466036+1nf0rmatix@users.noreply.github.com>
2022-04-01 10:49:41 +0200

    experimenting with github action

--- lib/src/reliable_interval_timer.dart
+++ lib/src/reliable_interval_timer.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:isolate';
 
 class ReliableIntervalTimer {
-  static const _isolateTimerDurationMicroseconds = 500;
+  static const _isolateTimerDurationMicroseconds = 250;
 
   /// Specifies the time that should lie in between execution of [callback]. Must not be smaller then one millisecond.
   final Duration interval;
